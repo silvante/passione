ab93fe5a9c911cfcb8fac53218bdc153d2b0ec5e
silvante <khamidov.ko@gmail.com>
2026-06-02 00:02:34 +0500

    Started a server

---
+++ lib/passione/api/server.rb
@@ -0,0 +1,15 @@
+require "sinatra/base"
+
+module Passione::API
+  class Server < Sinatra::Base
+    set :port, 4000
+    set :bind, "0.0.0.0"
+    
+    # --- actial routes
+    
+    get "/" do
+      content_type :json
+      return {status: "OK"}.to_json
+    end
+  end
+end
--- lib/passione/cli.rb
+++ lib/passione/cli.rb
@@ -1,16 +1,24 @@
 require "thor"
+require "passione/api/server"
 
 module Passione
   class CLI < Thor
 
     desc "start", "Starts the server"
     def start
-      puts "🚀 Starting the server..."
+      start_server
     end
 
     desc "version", "Shows the current version"
     def version
       puts "v#{VERSION}"
+    end
+
+    private
+
+    def start_server
+      puts "🚀 Starting sinatra server..."
+      API::Server.run!
     end
   end
 end

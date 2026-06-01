4954a9dba491bb12878537bdc7e1028d4298ac00
silvante <khamidov.ko@gmail.com>
2026-06-01 21:49:56 +0500

    Creating ruby gem

---
+++ lib/passione.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "passione/version"
+
+module Passione
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/passione/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Passione
+  VERSION = "0.1.0"
+end

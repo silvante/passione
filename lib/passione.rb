2fc711e1d636d62d2c1de318dc177f9d04dd7166
silvante <khamidov.ko@gmail.com>
2026-06-01 22:19:38 +0500

    First executable program

--- lib/passione.rb
+++ lib/passione.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "passione/version"
+require_relative "passione/cli"
 
 module Passione
   class Error < StandardError; end
---
+++ lib/passione/cli.rb
@@ -0,0 +1,7 @@
+module Passione
+  class CLI
+    def self.start
+      puts "🚀 My CI Server started"
+    end
+  end
+end

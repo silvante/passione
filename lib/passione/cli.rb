ef1e15f444d8651749c0242b6be8dbf0cf29e20b
silvante <khamidov.ko@gmail.com>
2026-06-01 23:37:57 +0500

    wow! building cli tools with ruby is awesome

--- lib/passione/cli.rb
+++ lib/passione/cli.rb
@@ -1,7 +1,16 @@
+require "thor"
+
 module Passione
-  class CLI
-    def self.start
-      puts "🚀 My CI Server started"
+  class CLI < Thor
+
+    desc "start", "Starts the server"
+    def start
+      puts "🚀 Starting the server..."
+    end
+
+    desc "version", "Shows the current version"
+    def version
+      puts "v#{VERSION}"
     end
   end
 end
